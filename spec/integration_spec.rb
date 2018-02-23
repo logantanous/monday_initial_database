93426593981b9019ab7b7e578ba044a534087eaa
Logan Tanous <logantanous@gmail.com>
2018-02-22 21:13:37 -0800

    got all tests to pass

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -15,7 +15,7 @@ end
 
 describe('viewing all of the lists', {:type => :feature}) do
   it('allows a user to see all of the lists that have been created') do
-    list = List.new({:name => 'Epicodus Homework'})
+    list = List.new({:name => 'Epicodus Homework', :id => nil})
     list.save()
     visit('/')
     click_link('View All Lists')
@@ -26,7 +26,7 @@ end
 
 describe('seeing details for a single list', {:type => :feature}) do
   it('allows a user to click a list to see the tasks and details for it') do
-    test_list = List.new({:name => 'School stuff'})
+    test_list = List.new({:name => 'School stuff', :id => nil})
     test_list.save()
     test_task = Task.new({:description => "learn SQL", :list_id => test_list.id()})
     test_task.save()
@@ -39,10 +39,12 @@ end
 
 describe('adding tasks to a list', {:type => :feature}) do
   it('allows a user to add a task to a list') do
-    test_list = List.new({:name => 'School stuff'})
+    test_list = List.new({:name => 'School stuff', :id => nil})
+    # binding.pry
     test_list.save()
     visit("/lists/#{test_list.id()}")
-    fill_in("Description", {:with => "Learn SQL"})
+    fill_in("description", {:with => "Learn SQL"})
+    fill_in("list_id", {:with => "#{test_list.id()}"})
     click_button("Add task")
     expect(page).to have_content("Success")
   end
